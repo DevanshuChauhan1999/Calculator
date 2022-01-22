ba3077949b80e3a60fe14372b23aa8ee0b9e0ad3
devanshuchauhan1999 <chauhandevanshu121@gmail.com>
2022-01-22 18:55:34 +0530

    Completed

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
+@SuppressLint("StaticFieldLeak")
     private var tvInput: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDot : Boolean = false
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +24,106 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
-
-
+        lastNumeric= true
+        lastDot = false
 
     //Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
-
     }
 
     fun onClear(view:View){
         tvInput?.text=""
+    }
+
+    fun onDecimalPoint(view:View){
+        if(lastNumeric && !lastDot)
+        {
+            tvInput?.append(".")
+            lastNumeric= false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix=""
+            try {
+                if (tvValue.startsWith("-")){
+                    prefix= "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one  = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    var result = one.toDouble() - two.toDouble()
+                    tvInput?.text = removeZeroAfterDot(result.toString())
+                }
+                else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one  = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    //var result = one.toDouble() + two.toDouble()
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one  = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    var result = one.toDouble() * two.toDouble()
+                    tvInput?.text = removeZeroAfterDot(result.toString())
+                }
+                else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one  = splitValue[0]
+                    var two = splitValue[1]
+                    if (prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    var result = one.toDouble() / two.toDouble()
+                    tvInput?.text = removeZeroAfterDot(result.toString())
+                }
+
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result : String): String {
+        var value = result
+        if(result.contains(".0"))
+            value= result.substring(0,result.length-2)
+        return value
+    }
+
+    private fun isOperatorAdded(value  : String):Boolean{
+        return if (value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
     }
 }
